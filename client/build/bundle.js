17f44e72f0a635187264589cff4f65ed7b5e9249
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-06-26 17:19:58 +0100

    adding all the things

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -111,6 +111,7 @@ UI.prototype = {
     for(var film of films) {
       var li = document.createElement("li");
       this.appendText(li, film.title, "Film: ");
+      this.appendText(li, film.genre, "Genre: ");
 
       for(var review of film.reviews){
         this.createReview(li, review);
@@ -135,23 +136,25 @@ var filmsSeedData = function(){
   var review1 = new Review({
     comment: "It's not even a thing. What just happened. I want my life back.",
     rating: 1,
-    author: "Craig"
+    author: "Craig",
   });
 
   var review2 = new Review({
     comment: "Pew pew pew lightsabers space cowboys whoot what's not to love",
     rating: 100,
-    author: "Harrison"
+    author: "Harrison",
   });
 
   var film1 = new Film({
     title: "Now You See Me",
-    actors: ["Woody Harrelson", "Jesse Eisenberg"]
+    actors: ["Woody Harrelson", "Jesse Eisenberg"],
+    genre: "drama"
   });
 
   var film2 = new Film({
     title: "Star Wars Episode IV: A New Hope",
-    actors: ["Harrison Ford", "Alec Guiness"]
+    actors: ["Harrison Ford", "Alec Guiness"],
+    genre: "sci-fi"
   });
 
   film1.addReview(review1);
@@ -171,6 +174,7 @@ var Film = function(options){
   this.title = options.title;
   this.actors = options.actors;
   this.reviews = options.reviews || [];
+  this.genre = options.genre;
 }
 
 Film.prototype = {
--- server.js
+++ server.js
@@ -8,14 +8,35 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var Film = require('./client/src/models/film');
 var Review = require('./client/src/models/review');
-var filmsSeedData = require('./client/src/models/films');
+var filmsSeedData = require('./client/src/models/films_seed_data');
 var films = filmsSeedData();
 
+app.delete( "/films/:id/", function( req, res ){
+  films.splice( films[req.params.id], 1);
+  res.json( {data: films } );
+})
 
+app.put( "/films/:id", function( req, res ){
+  films[req.params.id] = req.body.film;
+  res.json( { data: films } );
+})
+
+app.post( "/films", function( req, res ){
+  films.push( req.body.film );
+  res.json( { data: films } );
+})
+
+app.get( "/films/:id", function( req, res ){
+  res.json( { data: films[req.params.id] } );
+})
 
 app.get('/', function () {
-  res.sendFile(__dirname + '/client/build/index.html')
+  res.sendFile(__dirname + '/client/build/index.html');
 });
+
+app.get( "/films", function( req, res ) {
+  res.json( films );
+})
 
 app.listen(3000, function () {
   console.log('App running on port '+ this.address().port);
